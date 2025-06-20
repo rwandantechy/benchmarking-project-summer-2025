9aae01673bf67944db3ad0c2f8026e1806d8c051
Rwandantechy <niyinnocent2027@gmail.com>
2025-06-19 23:36:08 -0400

    fixed error with ram usage calculations

--- src/benchmark.py
+++ src/benchmark.py
@@ -46,8 +46,8 @@ def benchmark_model(model_name):
         "model": model_name,
         "response_time_sec": round(end_time - start_time, 2),
         "cpu_percent_used": round(cpu_after - cpu_before, 2),
-        "ram_used_mb": round((mem_after - mem_before) / (1024 * 1024), 2)
-    }
+        "ram_used_mb": round(abs(mem_after - mem_before) / (1024 * 1024), 2)
+ }
 
     return output, metrics
 
